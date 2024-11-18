e582f3afa4a548cff8ac17927fff066bfb1efb90
Raghav <raghav2004kaushal@gmail.com>
2024-11-18 21:44:28 +0530

    Modifications in navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -2,16 +2,18 @@ import React from 'react'
 import "../Styles/Navbar.css"
 const Navbar = () => {
   return (
-    <nav class="navbar">
-         <nav class="navbar">
-        <ul>
+    <>
+<nav class="navbar">
+        <div class="navbar-brand">MyWebsite</div>
+        <ul class="navbar-links">
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
             <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
         </ul>
     </nav>
-    </nav>
+
+    </>
   )
 }
 
